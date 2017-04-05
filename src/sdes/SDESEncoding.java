b22f1dce1c314ba3d1a8cfc59eba47c7f0c3433b
Ken Luo <luozhirong@gmail.com>
2017-04-04 23:19:49 -0700

    added some stuff.

---
+++ src/sdes/SDESEncoding.java
@@ -0,0 +1,37 @@
+package sdes;
+import sdes.CASCII;
+
+
+
+/**
+ * Created by kenluo on 4/4/17.
+ */
+public class SDESEncoding {
+
+    //    Give the SDES encoding of the following CASCII plaintext using the key 0111001101. (The answer
+//    is 64 bits long.)
+//    CRYPTOGRAPHY
+    byte [] key =  {0,1,1,1,0,0,1,1,0,1};
+
+    public static void main(String[] args) {
+        CASCII test;
+        test = new CASCII();
+
+        char[] testArray = {'H', 'E', 'L', 'L', 'O', 'Z'};
+
+        byte[] testByte = test.Convert(testArray);
+        for (int i = 0; i < testByte.length; i++) {
+            System.out.println(testByte[i]);
+        }
+
+//        String temp = test.toString(testByte);
+//
+//        byte[] holder = test.Convert(temp);
+//
+//        for (int i = 0; i < holder.length; i++) {
+//            System.out.println(holder[i]);
+//        }
+
+
+    }
+}
