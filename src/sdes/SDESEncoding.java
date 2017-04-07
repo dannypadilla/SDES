9e099aef77af9c7bcc11a294b5179467e59551c4
Ken Luo <luozhirong@gmail.com>
2017-04-06 21:22:35 -0700

    tried 3

--- src/sdes/SDESEncoding.java
+++ src/sdes/SDESEncoding.java
@@ -10,6 +10,7 @@ import sdes.SDES;
  */
 public class SDESEncoding {
 
+
     //    Give the SDES encoding of the following CASCII plaintext using the key 0111001101. (The answer
 //    is 64 bits long.)
 //    CRYPTOGRAPHY
@@ -21,87 +22,64 @@ public class SDESEncoding {
 
         char[] array = {'C', 'R', 'Y', 'P','T','O','G','R','A','P','H','Y'};
 
-        byte[][] paddedArray = new byte[array.length][8];
-
+        byte[][] convertedArray = new byte[array.length][5];
         for(int i = 0; i < array.length; i++){
-            paddedArray[i]= padding(cascii.convert(cascii.Convert(array[i])));
+            convertedArray[i] = cascii.convert(cascii.Convert(array[i]));
         }
 
-        byte[][] encrypted = new byte[paddedArray.length][paddedArray[0].length];
-        for(int i = 0; i < paddedArray.length; i++){
-            encrypted[i] = sdes.Encryption(key, paddedArray[i]);
+        //convert to 1d array
+        byte[] onedarray = new byte[convertedArray.length * convertedArray[0].length];
+        int counter = 0;
+        for(int i = 0; i < convertedArray.length; i++){
+            for(int k = 0; k < convertedArray[i].length; k++){
+                onedarray[counter] = convertedArray[i][k];
+                counter++;
+            }
         }
-        for(int i = 0; i < encrypted.length; i++){
-            sdes.print(encrypted[i]);
+
+        int holder = (array.length*5)%8;
+
+        byte[] newarrayPadded = new byte[array.length*5 + holder];
+        for(int i = 0; i < newarrayPadded.length; i++){
+            if(i < onedarray.length){
+                newarrayPadded[i] = onedarray[i];
+            }
+            else{
+                newarrayPadded[i] = 0;
+            }
+        }
+        System.out.println("new array padded");
+        sdes.print(newarrayPadded);
+        System.out.println();
+
+        //split back into 2d after padding
+        byte[][] split = new byte[newarrayPadded.length/8][8];
+        counter = 0;
+        for(int i = 0; i < split.length ;i++){
+            for(int k = 0; k < split[i].length; k++){
+                split[i][k] = newarrayPadded[counter];
+                counter++;
+            }
+        }
+        System.out.println("2d array after padding");
+        for(int i = 0; i < split.length; i++){
+            sdes.print(split[i]);
+            System.out.println();
         }
 
+        System.out.println();
+        byte[][] container = new byte[split.length][split[0].length];
+        for(int i = 0; i < split.length; i++){
+            container[i] = sdes.Encryption(key, split[i]);
+        }
 
-
-
-//        CASCII test;
-//        test = new CASCII();
-//
-//        char[] testArray = {'H', 'E', 'L', 'L', 'O', 'Z'};
-//
-//
-//        int l = 0;
-//        for (int counter = 31; counter != 0; counter--) {
-//            l = (int) Math.floor(Math.random() * (31 - 0) + 0);
-//            byte[] holder = test.convert(l);
-//            System.out.println(l);
-//            for (int i = 0; i < holder.length; i++) {
-//                System.out.print(holder[i] + " ");
-//
-//                byte[] testByte = test.Convert(testArray);
-//
-//                for (int i = 0; i < testByte.length; i++) {
-//                    for (int j = 0; j < test.convert(testByte[i]).length; j++) {
-//                        System.out.print(test.convert(testByte[i])[j]);
-//
-//                    }
-//                    System.out.println();
-//                }
-
-//        byte[] fiveBitsFromTxtFile = {1, 1, 1, 1, 1};
-//
-//        String fiveBitString = "";
-//
-//        for(int i = 0; i < fiveBitsFromTxtFile.length; i++) {
-//            fiveBitString += fiveBitsFromTxtFile[i];
-//        }
-//
-//        int fiveBitsToInt = Integer.parseInt(fiveBitString, 2);
-//
-//        System.out.println(fiveBitsToInt);
-//
-//        byte[] holder = test.convert(fiveBitsToInt);
-//        for(int i = 0; i < holder.length; i++){
-//            System.out.print(holder[i] + " ");
-//        }
-
-
-//        String temp = test.toString(testByte);
-//
-//        byte[] holder = test.Convert(temp);
-//
-//        for (int i = 0; i < holder.length; i++) {
-//            System.out.println(holder[i]);
-//        }
-
-
-            //}
-//        }
+        for(int i = 0; i < container.length; i++){
+            for(int k = 0; k < container[0].length; k++){
+                System.out.print(container[i][k]);
+            }
+        }
     }
-    public static byte[] padding(byte[] array){
-        byte[] padded = new byte[8];
-        for(int i = 0; i < 3; i++){
-            padded[i] = 0;
-        }
-        int counter = 3;
-        for(int i = 0; i < array.length; i++, counter++){
-            padded[counter] = array[i];
-        }
+    //0110000111001010101101101111010011000101011101101111110001110111
 
-        return padded;
-    }
+
 }
